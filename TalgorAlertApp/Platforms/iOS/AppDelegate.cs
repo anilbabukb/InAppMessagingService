f5763b7e41b4a14edb94567e69fa5752466213a2
preetha.sathyakantha <preetha.sathyakantha@mindze.com>
2022-08-21 13:00:21 +0530

    GoogleService-Info.plist added for apple

--- TalgorAlertApp/Platforms/iOS/AppDelegate.cs
+++ TalgorAlertApp/Platforms/iOS/AppDelegate.cs
@@ -1,4 +1,7 @@
 ﻿using Foundation;
+using UIKit;
+using FirebaseAdmin;
+
 
 namespace TalgorAlertApp
 {
@@ -6,5 +9,32 @@ namespace TalgorAlertApp
     public class AppDelegate : MauiUIApplicationDelegate
     {
         protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+        FirebasePushNotificationManager.Initialize(options,true);
+            public override void RegisteredForRemoteNotifications(UIApplication application, NSData deviceToken)
+        {
+            FirebasePushNotificationManager.DidRegisterRemoteNotifications(deviceToken);
+        }
+
+        public override void FailedToRegisterForRemoteNotifications(UIApplication application, NSError error)
+        {
+            FirebasePushNotificationManager.RemoteNotificationRegistrationFailed(error);
+
+        }
+        // To receive notifications in foregroung on iOS 9 and below.
+        // To receive notifications in background in any iOS version
+        public override void DidReceiveRemoteNotification(UIApplication application, NSDictionary userInfo, Action<UIBackgroundFetchResult> completionHandler)
+        {
+            // If you are receiving a notification message while your app is in the background,
+            // this callback will not be fired 'till the user taps on the notification launching the application.
+
+            // If you disable method swizzling, you'll need to call this method. 
+            // This lets FCM track message delivery and analytics, which is performed
+            // automatically with method swizzling enabled.
+            FirebasePushNotificationManager.DidReceiveMessage(userInfo);
+            // Do your magic to handle the notification data
+            System.Console.WriteLine(userInfo);
+
+            completionHandler(UIBackgroundFetchResult.NewData);
+        }
     }
 }
